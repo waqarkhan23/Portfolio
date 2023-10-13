51c3ac8823f38eee7e5761143311986dcfc673c5
Waqar Khan <waqar72642@gmail.com>
2023-10-13 21:59:36 +0500

    first commit

---
+++ src/Data/data.js
@@ -0,0 +1,34 @@
+import todo from "../assets/todo.png";
+import chat from "../assets/chatapp.png";
+import amazon from "../assets/Amazon.png";
+import calculator from "../assets/Calculator.png";
+export const data = [
+  {
+    id: 1,
+    name: "ToDo App (React Js)",
+    image: todo,
+    code: "",
+    demo: "",
+  },
+  {
+    id: 2,
+    name: "Chat App (React JS)",
+    image: chat,
+    code: "",
+    demo: "",
+  },
+  {
+    id: 3,
+    name: "Amazon Clone (HTML,CSS)",
+    image: amazon,
+    code: "",
+    demo: "",
+  },
+  {
+    id: 4,
+    name: "Calculator (React JS)",
+    image: calculator,
+    code: "",
+    demo: "https://www.calculator.net/",
+  },
+];
